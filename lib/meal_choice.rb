f3b083403c0d31d87c5168f2dee6a23e7e8a437a
Devone Austin <devone.austin@gmail.com>
2019-11-20 07:46:44 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,7 +4,7 @@
 def meal_choice(side1, side2, protein = "meat")
   puts "A plate of #{protein} with #{side1} and #{side2}."
   puts "What a nutritious meal!"
-  return protein
+  return meal_choice
 end
 
 meal_choice("broccoli", "macaroni")
